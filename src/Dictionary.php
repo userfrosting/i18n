5a7fdec104da2ce860439d86dede3ab7e202d8a8
Louis Charette <charette.louis@gmail.com>
2019-11-04 20:26:18 -0500

    Make Dictionary uses Support methods

--- src/Dictionary.php
+++ src/Dictionary.php
@@ -10,7 +10,7 @@
 
 namespace UserFrosting\I18n;
 
-use Illuminate\Support\Arr;
+use UserFrosting\Support\Repository\Repository;
 use UserFrosting\Support\Repository\Loader\ArrayFileLoader;
 use UserFrosting\Support\Repository\Loader\FileRepositoryLoader;
 use UserFrosting\UniformResourceLocator\ResourceLocatorInterface;
@@ -22,7 +22,7 @@ use UserFrosting\UniformResourceLocator\ResourceLocatorInterface;
  *
  * @author Louis Charette
  */
-class Dictionary implements DictionaryInterface
+class Dictionary extends Repository implements DictionaryInterface
 {
     /**
      * @var string Base URI for locator
@@ -47,7 +47,7 @@ class Dictionary implements DictionaryInterface
     /**
      * @var array Locale "Key => translation" data matrix cache
      */
-    protected $dictionary = [];
+    protected $items = [];
 
     /**
      * @param LocaleInterface          $locale
@@ -69,11 +69,11 @@ class Dictionary implements DictionaryInterface
      */
     public function getDictionary(): array
     {
-        if (empty($this->dictionary)) {
-            $this->dictionary = $this->loadDictionary();
+        if (empty($this->items)) {
+            $this->items = $this->loadDictionary();
         }
 
-        return $this->dictionary;
+        return $this->items;
     }
 
     /**
@@ -173,167 +173,5 @@ class Dictionary implements DictionaryInterface
     protected function getFiles(): array
     {
         return $this->locator->listResources($this->uri.$this->locale->getIndentifier(), true);
-    }
-
-    /**
-     * Determine if the given configuration value exists.
-     *
-     * @param string $key
-     *
-     * @return bool
-     */
-    public function has($key)
-    {
-        return Arr::has($this->all(), $key);
-    }
-
-    /**
-     * Get the specified configuration value.
-     *
-     * @param array|string $key
-     * @param mixed        $default
-     *
-     * @return mixed
-     */
-    public function get($key, $default = null)
-    {
-        if (is_array($key)) {
-            return $this->getMany($key);
-        }
-
-        return Arr::get($this->all(), $key, $default);
-    }
-
-    /**
-     * Get many configuration values.
-     *
-     * @param array $keys
-     *
-     * @return array
-     */
-    public function getMany($keys)
-    {
-        $config = [];
-
-        foreach ($keys as $key => $default) {
-            if (is_numeric($key)) {
-                [$key, $default] = [$default, null];
-            }
-
-            $config[$key] = Arr::get($this->all(), $key, $default);
-        }
-
-        return $config;
-    }
-
-    /**
-     * Set a given configuration value.
-     *
-     * @param array|string $key
-     * @param mixed        $value
-     *
-     * @return void
-     */
-    public function set($key, $value = null)
-    {
-        $keys = is_array($key) ? $key : [$key => $value];
-        $items = $this->all();
-
-        foreach ($keys as $key => $value) {
-            Arr::set($items, $key, $value);
-        }
-    }
-
-    /**
-     * Prepend a value onto an array configuration value.
-     *
-     * @param string $key
-     * @param mixed  $value
-     *
-     * @return void
-     */
-    public function prepend($key, $value)
-    {
-        $array = $this->get($key);
-
-        array_unshift($array, $value);
-
-        $this->set($key, $array);
-    }
-
-    /**
-     * Push a value onto an array configuration value.
-     *
-     * @param string $key
-     * @param mixed  $value
-     *
-     * @return void
-     */
-    public function push($key, $value)
-    {
-        $array = $this->get($key);
-
-        $array[] = $value;
-
-        $this->set($key, $array);
-    }
-
-    /**
-     * Get all of the configuration items for the application.
-     *
-     * @return array
-     */
-    public function all()
-    {
-        return $this->getDictionary();
-    }
-
-    /**
-     * Determine if the given configuration option exists.
-     *
-     * @param string $key
-     *
-     * @return bool
-     */
-    public function offsetExists($key)
-    {
-        return $this->has($key);
-    }
-
-    /**
-     * Get a configuration option.
-     *
-     * @param string $key
-     *
-     * @return mixed
-     */
-    public function offsetGet($key)
-    {
-        return $this->get($key);
-    }
-
-    /**
-     * Set a configuration option.
-     *
-     * @param string $key
-     * @param mixed  $value
-     *
-     * @return void
-     */
-    public function offsetSet($key, $value)
-    {
-        $this->set($key, $value);
-    }
-
-    /**
-     * Unset a configuration option.
-     *
-     * @param string $key
-     *
-     * @return void
-     */
-    public function offsetUnset($key)
-    {
-        $this->set($key, null);
     }
 }
